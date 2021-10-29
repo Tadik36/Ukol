8b11e261f38907e6bc2594d2e58ada3323e5894c
Tadik36 <91047426+Tadik36@users.noreply.github.com>
2021-10-29 11:38:25 +0200

    Delete src/druhaverze directory

--- src/druhaverze/Main.java
+++
@@ -1,9 +0,0 @@
-package druhaverze;
-
-public class Main {
-
-    public static void main(String[] args) {
-       Obsahkruhu obsah = new Obsahkruhu(5);
-        System.out.format("Obsah kruhu je: %.5f", obsah.obsah());
-    }
-}
